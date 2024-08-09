7ec350f2c0a3b2cf9cf8524d256290d46879ffaa
bansomin <good091231@126.com>
2024-08-10 00:51:55 +0800

    Updated bossComing

--- client/assets/scripts/ui/bossComing.js
+++ client/assets/scripts/ui/bossComing.js
@@ -2,13 +2,15 @@ var DataType = require("DataType");
 var bossComing = cc.Class({
     extends: cc.Component,
     properties: {
-        dragon: dragonBones.ArmatureDisplay,
-        box:cc.Node,
+        // dragon: dragonBones.ArmatureDisplay,
+        skeleton: sp.Skeleton,
+
+        box: cc.Node,
         monsterContainer: cc.Node,
         mask: cc.Node
     },
 
-    start(){
+    start() {
         if (window.winSize) {
             this.box.width = window.winSize.width / cc.view.getScaleX();
             this.box.height = window.winSize.height / cc.view.getScaleY();
@@ -17,24 +19,26 @@ var bossComing = cc.Class({
             this.box.height = window.innerHeight / cc.view.getScaleY();
         }
 
-        
-        this.dragon.on(dragonBones.EventObject.COMPLETE,this.onAnimComplete, this);
+
+        // this.dragon.on(dragonBones.EventObject.COMPLETE, this.onAnimComplete, this);
+        this.skeleton.setCompleteListener(this.onAnimComplete.bind(this))
     },
 
-    onAnimComplete:function(){
+    onAnimComplete: function () {
         setTimeout(() => {
-            this.zomObj.destroy()
+            if (this.zomObj) this.zomObj.destroy()
             this.node.active = false;
         }, 1000)
     },
 
-    playAnimation:function(_id) {
+    playAnimation: function (_id) {
         this.mask.setScale(cc.Mgr.game.isPad ? 1.35 : 1);
         var data = cc.Mgr.MapDataMgr.getDataByDataTypeAndKey(DataType.ZombieData, _id.toString());
         this.monsterContainer.x = -400
         this.box.opacity = 150;
-        cc.tween(this.box).to(0.2,{opacity:255}).to(0.2,{opacity:150}).to(0.2,{opacity:255}).to(0.2,{opacity:150}).start();
-        this.dragon.playAnimation("boss", 1);
+        cc.tween(this.box).to(0.2, { opacity: 255 }).to(0.2, { opacity: 150 }).to(0.2, { opacity: 255 }).to(0.2, { opacity: 150 }).start();
+        // this.dragon.playAnimation("boss", 1);
+        this.skeleton.setAnimation(0, "boss_coming", false)
         let self = this;
         cc.loader.loadRes("prefab/zombiesnew/" + data.prefab, function (errmsg, prefab) {
             if (errmsg) {
@@ -45,7 +49,7 @@ var bossComing = cc.Class({
             self.zomObj.parent = self.monsterContainer;
             var zombieScript = self.zomObj.getComponent("zombie");
             zombieScript.showComing();
-            cc.tween(self.monsterContainer).to(1, {position: cc.v2(-194, 350)}, {easing: "cubicOut"}).start();
+            cc.tween(self.monsterContainer).to(1, { position: cc.v2(-194, 350) }, { easing: "cubicOut" }).start();
         });
     },
 });
